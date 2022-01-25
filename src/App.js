a92bce207bf11e5ba026bd7f62a62887e73ed452
csdiehl <csdiehl16@gmail.com>
2022-01-24 19:39:02 -0800

    got chart to render on initial load

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import InputForm from "./components/InputForm";
 import BigNumber from "./components/BigNumber";
-import Chart from './components/Chart.js';
+import Chart from "./components/Chart.js";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-const DUMMY_DATA = [{
-  year: 1,
-  savings: 50000
-}, {year: 2, savings: 60000}, {year: 3, savings: 70000}]
+const DUMMY_DATA = [
+  {
+    year: 1,
+    savings: 50000,
+  },
+  { year: 2, savings: 60000 },
+  { year: 3, savings: 70000 },
+];
 
 function App() {
-
   const [data, setData] = useState({
     age: 30,
     netWorth: 60000,
@@ -21,55 +27,60 @@ function App() {
     retireStocks: 0,
     retireBonds: 0,
     otherStocks: 0,
-    otherBonds: 0
-  })
+    otherBonds: 0,
+  });
 
-  const [cash, setCash] = useState(47500)
-  const [target, setTarget] = useState(562500)
+  const [cash, setCash] = useState(47500);
+  const [target, setTarget] = useState(562500);
 
-  const [chartData, setChartData] = useState(DUMMY_DATA)
+  const [chartData, setChartData] = useState(DUMMY_DATA);
 
   const changeHandler = (event) => {
-    const target = event.target
-    const value = +target.value
-    const name = target.name
+    const target = event.target;
+    const value = +target.value;
+    const name = target.name;
 
-    setData((prevState) => {return {...prevState, [name]: value}} )
-  }
+    setData((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
 
   const calcInputs = () => {
-    const expenses = (data.housing + data.transportation + data.food)
-    const cashFlow = Math.round((data.salary - expenses))
-    const fireTarget = Math.round(expenses * 25)
+    const expenses = data.housing + data.transportation + data.food;
+    const cashFlow = Math.round(data.salary - expenses);
+    const fireTarget = Math.round(expenses * 25);
 
-    const years = Math.round((fireTarget - data.netWorth) / cashFlow)
+    const years = Math.round((fireTarget - data.netWorth) / cashFlow);
 
-    const yearData = []
-    let totalSavings = data.netWorth
+    const yearData = [];
+    let totalSavings = data.netWorth;
 
     for (let i = 1; i <= years; i++) {
-
       yearData.push({
         year: i,
-        savings: totalSavings
-      })
+        savings: totalSavings,
+      });
 
-      totalSavings += cashFlow
+      totalSavings += cashFlow;
     }
 
-    setCash(cashFlow)
-    setTarget(fireTarget)
-    setChartData(yearData)
-
-  }
-
+    setCash(cashFlow);
+    setTarget(fireTarget);
+    setChartData(yearData);
+  };
 
   return (
-    <div className="App">
-        <InputForm calcInputs = {calcInputs} changeHandler = {changeHandler} />
-        <BigNumber target = {target} cashFlow = {cash} />
-        <Chart data = {chartData}/>
-      </div>
+    <Container className="justify-content-md-center App">
+      <Row>
+        <Col md = "3">
+          <InputForm calcInputs={calcInputs} changeHandler={changeHandler} />
+        </Col>
+        <Col md = "9">
+          <BigNumber target={target} cashFlow={cash} />
+          <Chart data={chartData} />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,36 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as d3 from "d3";
 
 const Chart = (props) => {
+  const height = 500;
+  const width = 800;
+  const padding = 30;
 
-const height = 500
-const width = 800
-const padding = 30
+ 
+  useEffect(() => {
+   
+      const svg = d3.select("svg").attr("height", "500").attr("width", "800");
+  
+      const xScale = d3
+      .scaleLinear()
+      .domain([1, d3.max(props.data, (d) => d.year)])
+      .range([padding, width - padding]);
+  
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(props.data, (d) => d.savings)])
+      .range([height - padding, padding]);
+  
+    svg
+      .selectAll()
+      .data(props.data)
+      .enter()
+      .append("circle")
+      .attr("cx", (d) => xScale(d.year))
+      .attr("cy", (d) => yScale(d.savings))
+      .attr("r", 5)
+      .attr("fill", "#FFF");
+    
 
-const svg = d3.select('svg').attr('height', '500').attr('width', '800')
-
-const xScale = d3.scaleLinear()
-.domain([1, d3.max(props.data, d => d.year)])
-.range([padding, width - padding])
-
-const yScale = d3.scaleLinear()
-.domain([0, d3.max(props.data, d => d.savings)])
-.range([height - padding, padding])
-
-svg.selectAll()
-.data(props.data)
-.enter()
-.append('circle')
-    .attr('cx', d => xScale(d.year))
-    .attr('cy', d => yScale(d.savings))
-    .attr('r', 5)
-    .attr('fill', '#FFF')
+    return function() {
+      document.getElementById('chart').innerHTML = ''
+    }
+  }, [props.data]);
 
   return (
     <div>
       <h1>This will be a Chart</h1>
-      <svg></svg>
-      <p>{JSON.stringify(props.data)}</p>
+      <svg id="chart"></svg>
     </div>
   );
 };
