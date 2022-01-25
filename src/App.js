be12cc0824c08f08907371ceada7bbc637690ea7
csdiehl <csdiehl16@gmail.com>
2022-01-24 17:02:47 -0800

    chart added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,74 @@
 import "./App.css";
 import React, {useState} from 'react';
 import InputForm from "./components/InputForm";
+import BigNumber from "./components/BigNumber";
+import Chart from './components/Chart.js';
+
+const DUMMY_DATA = [{
+  year: 1,
+  savings: 50000
+}, {year: 2, savings: 60000}, {year: 3, savings: 70000}]
 
 function App() {
 
-  [data, setData] = useState({
-    age: 27,
+  const [data, setData] = useState({
+    age: 30,
     netWorth: 60000,
     salary: 70000,
-    food: 0,
-    housing: 0,
-    transportation: 0,
+    food: 6000,
+    housing: 15000,
+    transportation: 1500,
     retireStocks: 0,
     retireBonds: 0,
     otherStocks: 0,
     otherBonds: 0
   })
 
-  const changeHandler = () => {
+  const [cash, setCash] = useState(47500)
+  const [target, setTarget] = useState(562500)
+
+  const [chartData, setChartData] = useState(DUMMY_DATA)
+
+  const changeHandler = (event) => {
+    const target = event.target
+    const value = +target.value
+    const name = target.name
+
+    setData((prevState) => {return {...prevState, [name]: value}} )
+  }
+
+  const calcInputs = () => {
+    const expenses = (data.housing + data.transportation + data.food)
+    const cashFlow = Math.round((data.salary - expenses))
+    const fireTarget = Math.round(expenses * 25)
+
+    const years = Math.round((fireTarget - data.netWorth) / cashFlow)
+
+    const yearData = []
+    let totalSavings = data.netWorth
+
+    for (let i = 1; i <= years; i++) {
+
+      yearData.push({
+        year: i,
+        savings: totalSavings
+      })
+
+      totalSavings += cashFlow
+    }
+
+    setCash(cashFlow)
+    setTarget(fireTarget)
+    setChartData(yearData)
 
   }
 
+
   return (
     <div className="App">
-        <InputForm changeHandler = {changeHandler} />
+        <InputForm calcInputs = {calcInputs} changeHandler = {changeHandler} />
+        <BigNumber target = {target} cashFlow = {cash} />
+        <Chart data = {chartData}/>
       </div>
   );
 }
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import * as d3 from 'd3';
+import React from "react";
+import * as d3 from "d3";
 
-const Chart = () => {
+const Chart = (props) => {
 
-}
+const height = 500
+const width = 800
+const padding = 30
+
+const svg = d3.select('svg').attr('height', '500').attr('width', '800')
+
+const xScale = d3.scaleLinear()
+.domain([1, d3.max(props.data, d => d.year)])
+.range([padding, width - padding])
+
+const yScale = d3.scaleLinear()
+.domain([0, d3.max(props.data, d => d.savings)])
+.range([height - padding, padding])
+
+svg.selectAll()
+.data(props.data)
+.enter()
+.append('circle')
+    .attr('cx', d => xScale(d.year))
+    .attr('cy', d => yScale(d.savings))
+    .attr('r', 5)
+    .attr('fill', '#FFF')
+
+  return (
+    <div>
+      <h1>This will be a Chart</h1>
+      <svg></svg>
+      <p>{JSON.stringify(props.data)}</p>
+    </div>
+  );
+};
 
 export default Chart;
